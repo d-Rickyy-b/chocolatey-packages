8e826497ecbdf95e01b1d7bda78bdb1dbbbda45a
Rico <d-Rickyy-b@users.noreply.github.com>
2023-08-12 21:58:47 +0200

    feat: add f2 chocolatey package

---
+++ f2/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url32 = 'https://github.com/ayoisaiah/f2/releases/download/v1.9.1/f2_1.9.1_windows_386.tar.gz'
+$checksum32 = '92bc9b00d88356c26b947b9a75b87bdb751385665c606e70ce31c49459612fb1'
+$url64 = 'https://github.com/ayoisaiah/f2/releases/download/v1.9.1/f2_1.9.1_windows_amd64.tar.gz'
+$checksum64 = '5c4a3d825c7fb4d371d3fb8e0b987868b07b279047c507ebef1c1cc6c59e9469'
+
+$installArgs = @{
+    packageName   = 'f2'
+    unzipLocation = $toolsDir
+    fileType      = 'GZIP'
+    url           = $url32
+    checksumType  = 'sha256'
+    checksum      = $checksum32
+    url64bit      = $url64
+    checksumType64   = 'sha256'
+    checksum64       = $checksum64
+}
+
+Install-ChocolateyZipPackage @installArgs
+$File = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter *.tar
+Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $env:ChocolateyInstall\lib\$packageName\tools\
---
+++ f2/update.ps1
@@ -0,0 +1,39 @@
+Import-Module au
+
+function global:au_GetLatest {
+    $response = Invoke-WebRequest "https://api.github.com/repos/ayoisaiah/f2/releases"
+    $releases = $response | ConvertFrom-Json
+    $latest_release = $releases | Where-Object {$_.prerelease -eq $false} | Select-Object -First 1
+
+    $version = $latest_release.tag_name.TrimStart("v")
+
+    foreach ($asset in $latest_release.assets) {
+        $windows_asset_32 = $asset | Where-Object name -Match '_windows_(i?)386.tar.gz'
+        $windows_asset_64 = $asset | Where-Object name -Match '_windows_amd64.tar.gz'
+        if ($windows_asset_32) {
+            $download_url_32 = $windows_asset_32.browser_download_url
+        }
+        if ($windows_asset_64) {
+            $download_url_64 = $windows_asset_64.browser_download_url
+        }
+    }
+    
+    return @{
+        Version = $version
+        URL32   = $download_url_32
+        URL64   = $download_url_64
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\`$url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(?i)(^\`$checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\`$url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(?i)(^\`$checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update
