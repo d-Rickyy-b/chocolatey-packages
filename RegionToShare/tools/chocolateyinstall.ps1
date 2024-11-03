4f31eb54dd247ff28706d4add9baa9d1d5d536f8
Chocolatey <chocolatey@realdimensions.net>
2024-11-03 04:34:48 +0000

    AU: 1 updated - RegionToShare [skip ci]

--- RegionToShare/tools/chocolateyinstall.ps1
+++ RegionToShare/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$downloadUrl = 'https://github.com/tom-englert/RegionToShare/releases/download/1.10/RegionToShare.zip'
-$fileChecksum = '79d19915cf6305429ae668bafc01d9ea85ff50a9ca596770431b75109bd65a63'
+$downloadUrl = 'https://github.com/tom-englert/RegionToShare/releases/download/1.11.0/RegionToShare.zip'
+$fileChecksum = '47f08f0d27a50d656e6ba08320a6001c038ffe85ff1fdacc1ab28108fb3929e7'
 
 $installArgs = @{
     packageName   = 'regiontoshare'
