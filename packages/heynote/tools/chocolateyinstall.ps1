6da23d057189a75ca79eb975d1e5bd479dc1c05e
Chocolatey <chocolatey@realdimensions.net>
2026-02-15 05:06:33 +0000

    AU: 1 updated - heynote [skip ci]

--- packages/heynote/tools/chocolateyinstall.ps1
+++ packages/heynote/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://github.com/heyman/heynote/releases/download/v2.8.1/Heynote_2.8.1.exe'
-$checksum = '94fb9f08e1f5ae7a372eb4060e0cd2918fbbe76dc7e29f74185eef9b55de162f'
+$url = 'https://github.com/heyman/heynote/releases/download/v2.8.2/Heynote_2.8.2.exe'
+$checksum = 'be58acadf31999c64967f6dc9c0774b947d0b23c682e21f2fb47a196b11a4c1a'
 
 $installArgs = @{
     packageName   = 'heynote'
